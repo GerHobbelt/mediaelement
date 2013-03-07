3485af3f20dc59c3b9fa5881650fd310cc5d8d04
John Harman <john@johnharman.co.uk>
2013-03-07 12:21:39 +0000

    Altered the google tracking code to use our own function.

--- src/js/mep-feature-googleanalytics.js
+++ src/js/mep-feature-googleanalytics.js
@@ -10,59 +10,33 @@ $.extend(mejs.MepDefaults, {
 	googleAnalyticsTitle: '',
 	googleAnalyticsCategory: 'Videos',
 	googleAnalyticsEventPlay: 'Play',
-	googleAnalyticsEventPause: 'Pause',
 	googleAnalyticsEventEnded: 'Ended',
-	googleAnalyticsEventTime: 'Time'
+	googleAnalyticstrackingFunction: null
 });
-
 
 $.extend(MediaElementPlayer.prototype, {
 	buildgoogleanalytics: function(player, controls, layers, media) {
-			
+
 		media.addEventListener('play', function() {
 			if (typeof _gaq != 'undefined') {
-				_gaq.push(['_trackEvent', 
-					player.options.googleAnalyticsCategory, 
-					player.options.googleAnalyticsEventPlay, 
+				player.options.googleAnalyticstrackingFunction(['_trackEvent',
+					player.options.googleAnalyticsCategory,
+					player.options.googleAnalyticsEventPlay,
 					(player.options.googleAnalyticsTitle === '') ? player.currentSrc : player.options.googleAnalyticsTitle
 				]);
 			}
 		}, false);
-		
-		media.addEventListener('pause', function() {
-			if (typeof _gaq != 'undefined') {
-				_gaq.push(['_trackEvent', 
-					player.options.googleAnalyticsCategory, 
-					player.options.googleAnalyticsEventPause, 
-					(player.options.googleAnalyticsTitle === '') ? player.currentSrc : player.options.googleAnalyticsTitle
-				]);
-			}
-		}, false);	
-		
+
 		media.addEventListener('ended', function() {
 			if (typeof _gaq != 'undefined') {
-				_gaq.push(['_trackEvent', 
-					player.options.googleAnalyticsCategory, 
-					player.options.googleAnalyticsEventEnded, 
+				player.options.googleAnalyticstrackingFunction(['_trackEvent',
+					player.options.googleAnalyticsCategory,
+					player.options.googleAnalyticsEventEnded,
 					(player.options.googleAnalyticsTitle === '') ? player.currentSrc : player.options.googleAnalyticsTitle
 				]);
 			}
 		}, false);
-		
-		/*
-		media.addEventListener('timeupdate', function() {
-			if (typeof _gaq != 'undefined') {
-				_gaq.push(['_trackEvent', 
-					player.options.googleAnalyticsCategory, 
-					player.options.googleAnalyticsEventEnded, 
-					player.options.googleAnalyticsTime,
-					(player.options.googleAnalyticsTitle === '') ? player.currentSrc : player.options.googleAnalyticsTitle,
-					player.currentTime
-				]);
-			}
-		}, true);
-		*/
 	}
 });
-	
+
 })(mejs.$);
